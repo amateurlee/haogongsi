98971d8be832566c6eadad4188900db522988b9c
amateurlee <amateurlee@sina.com>
2015-02-14 16:11:44 +0000

    add admin css static file

--- commitToHeroKu.sh
+++ commitToHeroKu.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo 'git add --all'
+echo '========> git add --all'
 git add --all
-echo 'git commit -a -m "$1"'
+echo '========> git commit -a -m "$1"'
 git commit -a -m "$1"
-echo 'git push heroku master'
+echo '========> git push heroku master'
 git push heroku master
