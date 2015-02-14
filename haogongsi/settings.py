65fb29f59968ed83327d84456a5df6711354cd38
amateurlee <amateurlee@sina.com>
2015-02-14 16:36:36 +0000

    switch STATIC_ROOT and STATIC_URL variable order

--- haogongsi/settings.py
+++ haogongsi/settings.py
@@ -82,11 +82,9 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_PATH, 'templates'),
 )
-
-MEDIA_ROOT = 'media'
